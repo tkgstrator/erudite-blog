1fbbc73308dcf9e0240b98c8a7dfdaad534a7711
Plat <60182057+p1atdev@users.noreply.github.com>
2025-07-12 21:23:24 +0900

    feat: theme hue slider

---
+++ src/components/theme/dynamicHue.ts
@@ -0,0 +1,42 @@
+export const DEFAULT_HUE = 256 as const;
+
+export const applyThemeHue = (angle: number) => {
+  const dataTheme =
+    document.documentElement.getAttribute("data-theme") ?? "light";
+
+  const properties = {
+    light: {
+      background: `oklch(100% 0.01 ${angle}deg)`,
+      foreground: `oklch(14.5% 0.05 ${angle}deg)`,
+      secondary: `oklch(97% 0.01 ${angle}deg)`,
+      border: `oklch(92.2% 0.015 ${angle}deg)`,
+      input: `oklch(92.2% 0.015 ${angle}deg)`,
+    },
+    dark: {
+      background: `oklch(20% 0.01 ${angle}deg)`,
+      foreground: `oklch(98.5% 0.03 ${angle}deg)`,
+      secondary: `oklch(25% 0.01 ${angle}deg)`,
+      border: `oklch(25% 0.015 ${angle}deg)`,
+      input: `oklch(25% 0.015 ${angle}deg)`,
+    },
+  };
+  if (!Object.keys(properties).includes(dataTheme)) {
+    console.warn(`Theme "${dataTheme}" not found. Using default properties.`);
+    return;
+  }
+
+  Object.entries(properties[dataTheme as keyof typeof properties]).forEach(
+    ([key, value]) => {
+      document.documentElement.style.setProperty(`--${key}`, value);
+    },
+  );
+};
+
+export const storeHue = (angle: number) => {
+  localStorage.setItem("theme-hue", angle.toString());
+};
+
+export const getHue = () => {
+  const storedHue = localStorage.getItem("theme-hue");
+  return storedHue ? parseFloat(storedHue) : DEFAULT_HUE; // Default to 256 if not set
+};
