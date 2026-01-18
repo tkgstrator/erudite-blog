cf6ae9e75a743e7e31785b2a50918046f26b8b00
tkgstrator <nasawake.am@gmail.com>
2026-01-19 05:25:18 +0900

    -----BEGIN PGP SIGNED MESSAGE----- Hash: SHA256
    feat: update devcontainer features
    -----BEGIN PGP SIGNATURE-----
    
    iQIzBAEBCAAdFiEEEAjnYmSHDtVyIminyd6ZHRpSJHgFAmltWnAACgkQyd6ZHRpS
    JHgixg/8Dp/30BWR0BjWmCq7kQ5ZLHKTvM1yWeD7afyZA2buNSNqmKXfYxcG8l7r
    zouC5hC5NHNstCdJg9KcJy/KtVsC6ojg/G9J8T8Z3Q49NmZNoTS1YXn8bLnJHQ/w
    wkzmIPkOi2RtDfXZR/OIFCd0wiYw88fkxtoM5iNnWoN/2Uoikk44J6ZXZLGtbz6G
    BxFkhlUwHsGdHhSkDEsr3LWq7KJn3ZmHMAmS3YWig1cify4ToikNUwzghbyEYEgh
    p0VShQhMxKBg4UkudRz+tBJrMBVPL9hkLhJy/5FA5ci7LZoJmY0NF1qlQbK0wVFJ
    8PZ0PlAPg0ln4rzfMQh+/MDoxQFqUNYb6w8xLbbJL0BrpT4ft4A4TXtYK36rLsoP
    L0XW9AGMO2BnZ2vwTqdLerlUqcAbCX87TUmy0439HpVvp3v/+5rNAA0SwyJI3EPM
    W4KKsccLn1Ci1Pyr5hjadlBpXJKTBKLvyykDwlH/yE7Ny9306zhB5/ovBWiieZa3
    5dOtJPSawy248TZt7lyiE88/rb1NMcde/USXKrrw9XxPZg13OCLWE0WOm2J+8Mqh
    +/ddR8OLF6pCQmmj7H8oNLkP0qiyEQFE6S3ApOSZD723JfO2qr60TLWOl+BZ6AHC
    br2NYf1w7WgxrzJSvSeCVBQzsMIM+AYaAU0aDKtWFfruyHSzWrY=
    =+df2
    -----END PGP SIGNATURE-----

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# GPG_TTYを設定
+echo 'export GPG_TTY=$(tty)' >> /home/vscode/.zshrc
+
 sudo chown -R vscode:vscode node_modules
 sudo chown -R vscode:vscode .astro
 sudo chown -R vscode:vscode dist
