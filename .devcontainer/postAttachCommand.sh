b00c9025e7f88a21a771265e88ef37fbdc452f2f
tkgstrator <nasawake.am@gmail.com>
2026-01-24 00:44:17 +0000

    feat: update astro config and blog theme

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -7,3 +7,6 @@ git config --global --add --bool push.autoSetupRemote true
 git config --global commit.gpgSign true
 git config --global user.signingkey $(gpg --list-secret-keys --with-colons | grep -B 3 "uid.*$(git config user.name)" | cut -d: -f5 | sed ':a;N;$!ba;s/\n//g')
 git branch --merged|egrep -v '\*|develop|main|master'|xargs git branch -d
+
+bun install --frozen-lockfile --ignore-scripts
+bunx --bun biome migrate --write
---
+++ .vscode/scripts/deploy.sh
@@ -0,0 +1,1 @@
+act -j deploy --container-architecture=linux/arm64 -P ubuntu-24.04=catthehacker/ubuntu:act-22.04 --secret-file .secrets
